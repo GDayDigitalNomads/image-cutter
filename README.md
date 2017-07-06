b4055a3ea155cb935dbee4138b51c0467d2959c3
Jordan Rancie <jordan@thetradingbox.com>
2017-07-06 11:01:35 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,65 @@
+# Image Cutter
+
+A simple image cutter tool for individual and bulk image cutting for better performance of websites.
+
+#### Installation
+
+    $ npm install
+    $ npm link
+    
+    
+#### usage
+
+    $ image-cutter <image-file || directory> [OPTIONS]
+    
+         OPTIONS:
+             --width <value> (Optional: will respect width/heigh ratio)
+             --height <value> (Optional: will respect width/heigh ratio)
+             --maxwidth (Optional: Crop if --width is omitted, and final width is > maxwidth)
+             --maxheight (Optional: Crop if --height is omitted, and final height is > maxheight)
+             --crop (Optional 0-100: 0 = crop from top,left, 100% = crop from bottom,right. Default is 50.)
+             --prefix <value> (Optional: add prefix to output file name)
+             --suffix <value> (Optional: add suffix to output file name)             
+             --name <value> (Optional: new image name - overrides --prefix and --suffix)
+             --dir <value> (Optional: output file(s) to directory)
+             --jekyll <value> (Optional: create a .yml of results)
+                 
+                 
+##### Examples:
+
+    
+        $ ./image-cutter imagedirectory --width 200 --dir width200
+        # Resizes all images in "imagedirectory"  to a width of 200px (height/width ratio kept) 
+        # output results to "imagedirectory/width200"
+    
+        $ ./image-cutter imagedirectory --height 200 --maxwidth 200 --crop 100 --dir height200
+        # Resizes all images in "imagedirectory" to a height of 200px (height/width ratio kept)
+        # The crops all resulting images so they are no larger than 200px in height, removing the top of the image.
+        # output results to "imagedirectory/height200"
+            
+        $ ./image-cutter image.jpg --height 200 --width 200 --name newimage.jpg
+        # Resizes image.jpg to a width/height of 200px
+        # outputs to newimage.jpg
+           
+                
+The MIT License (MIT)
+
+>
+>Copyright (c) Jordan Rancie
+>
+>Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+>
+>The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+>
+>THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.        
---
+++ index.js
@@ -0,0 +1,183 @@
+#! /usr/bin/env node
+var image = require('lwip')
+//var Queue = require('better-queue');
+var fs = require('fs');
+var args = require('minimist')(process.argv.slice(2));
+
+// obtain an image object:
+var allowed_ext = [".jpg", ".jpeg", ".gif", ".png"]
+var stream
+
+var processImage = function(dir, imgfilename) {
+    var fn = imgfilename.substring(0, imgfilename.lastIndexOf('.'));
+    var ext = imgfilename.substring(imgfilename.lastIndexOf('.'), imgfilename.length).toLowerCase();
+    if (!allowed_ext.includes(ext)) {
+        console.log('Skipping %s...unsupported image....', imgfilename)
+        return;
+    }
+    var newfilename = ''
+    var newfilenamePath = dir !== '' ? dir + '/' : ''
+    if (args.name) {
+        newfilename = args.name
+    } else {
+        if (args.prefix)
+            newfilename = args.prefix + newfilename;
+        newfilename = newfilename + fn;
+        if (args.suffix)
+            newfilename = newfilename + args.suffix;
+        newfilename = newfilename + ext;
+    }
+
+    if (args.d || args.dir) {
+        var newdir = newfilenamePath + ( args.d ? args.d : args.dir )
+        if (!fs.existsSync(newdir))
+            fs.mkdirSync(newdir);
+        newfilename = newdir + '/' + newfilename
+    }
+
+    image.open(newfilenamePath + imgfilename, function (err, img) {
+        if (err) {
+            console.error("Woops: %s", err.message)
+            return;
+        }
+
+        var currWidth = img.width()
+        var currHeight = img.height()
+        var newHeight = currHeight
+        var newWidth = currWidth
+        var resize = false;
+        var cropWidth = false;
+        var cropHeight = false;
+        if(args.width && !(args.height)) {
+            newWidth = args.width
+            newHeight = (newWidth / currWidth) * currHeight
+            resize = true
+            if (args.maxheight && (newHeight > args.maxheight))
+                cropHeight = true
+        } else if(args.height && !(args.width)) {
+            newHeight = args.height
+            newWidth = (newHeight / currHeight) * currWidth
+            resize = true;
+            if (args.maxwidth && (newWidth > args.maxwidth))
+                cropWidth = true
+        } else if (args.height && args.width) {
+            newWidth = args.width
+            newHeight = args.height
+            resize = true;
+        }
+
+        if (resize) {
+            console.log("Resizing %s (w:%s,h%s) to %s (w:%s,h:%s)",  newfilenamePath + imgfilename, currWidth, currHeight, newfilename, newWidth, newHeight)
+            img.resize(newWidth, newHeight, "lanczos", function (err, imgResized) {
+                if (err) {
+                    console.error('Error resizing %s. %s', imgResized, err.message)
+                    return;
+                }
+
+                if (cropWidth || cropHeight) {
+                    var top = 1
+                    var bottom = newHeight
+                    var left = 1
+                    var right = newWidth
+                    var align = args.crop || args.crop == 0 ? args.crop : 50
+
+                    if (cropWidth) {
+                        var a = (newWidth - args.maxwidth)
+                        var perc = (align / 100)
+                        var _perc = (1 - perc)
+                        left =  (a * perc) + 1
+                        right = newWidth - (a * _perc)
+                    }
+                    if (cropHeight) {
+                        var a = (newHeight - args.maxheight)
+                        var perc = (align / 100)
+                        var _perc = (1 - perc)
+                        top = (a * perc) + 1
+                        bottom = newHeight - (a * _perc)
+                    }
+
+                    console.log("Cropping %s (w:%s,h%s) to %s (w:%s,h:%s)",  newfilenamePath + imgfilename, currWidth, currHeight, newfilename, (right - left) + 1, (bottom - top) + 1)
+
+                    imgResized.crop(left, top, right, bottom, function (err, imgCropped) {
+                        if(err) {
+                            console.error('Error cropping %s. %s', imgCropped, err.message)
+                            return;
+                        }
+                        imgCropped.writeFile(newfilename, function(err){
+                            if (err) {
+                                console.error('Write error: %s', err.message)
+                            } else if (dir && dir != '') {
+                                stream.write('   - url: ' + imgfilename + '\n')
+                            }
+                        });
+                    })
+
+                } else {
+                    imgResized.writeFile(newfilename, function(err){
+                        if (err) {
+                            console.log('Write error: %s', err.message)
+                        } else if (dir && dir != '' && args.jekyll) {
+                            stream.write('   - url: ' + imgfilename + '\n')
+                        }
+                    });
+                }
+            })
+
+        }
+    })
+}
+
+
+//console.log('./image-cutter tool.');
+
+if (!args._ || args._.length == 0) {
+    console.log('Unknown usage.')
+    console.log('./image-cutter <image-file || directory> {image-file1}.... [OPTIONS]')
+    console.log('         OPTIONS:')
+    console.log('         --width <value> (Optional: If OMITTED will resize to respect width/heigh ratio)')
+    console.log('         --height <value> (Optional: If OMITTED will resize to respect width/heigh ratio)')
+    console.log('         --maxwidth (Optional: crop, if --width is omitted)')
+    console.log('         --maxheight (Optional: crop, if --height is omitted)')
+    console.log('         --crop (Optional 0-100: 0 = top,left, 100% = bottom,right. Default is 50.)')
+    console.log('         --name <value> (Optional: new image name - overrides --prefix and --suffix)')
+    console.log('         --dir <value> (Optional: output to directory)')
+    console.log('         --prefix <value> (Optional: add prefix to output file name)')
+    console.log('         --suffix <value> (Optional: add suffix to output file name)')
+    console.log('         --jekyll <value> (Optional: create a jekyll yml file for the conversion)')
+    return;
+}
+
+
+args._.forEach(function (imgfilename) {
+
+    if (fs.existsSync(imgfilename) && fs.statSync(imgfilename).isDirectory()) {
+        console.log('%s is a directory...converting all images....', imgfilename)
+        if (args.jekyll) {
+            var outputFileName = (args.dir ? args.dir + '_' : '') + 'gallery_.yml'
+            var out = ' - title: ' + imgfilename + '\n' +
+                '   directory: ' + imgfilename + '\n' +
+                '   thumb: ' + (args.dir ? args.dir : '') + '\n' +
+                '   images: \n'
+            stream = fs.createWriteStream(imgfilename + '/' + outputFileName, {flags: 'w'});
+            stream.write(out);
+            var files = fs.readdirSync(imgfilename);
+        }
+        files.forEach((function(file) {
+            //console.log('Processing %s', imgfilename + '/' + file)
+            processImage(imgfilename, file)
+        }))
+
+    } else {
+        processImage('', imgfilename)
+    }
+})
+
+//process.exit(0);
+
+
+
+
+
+
+
+
